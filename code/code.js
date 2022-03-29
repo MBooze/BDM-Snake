5067a271b27970f2339a1ea57984627c227a69da
Booze <61169560+MBooze@users.noreply.github.com>
2022-03-28 20:59:52 -0400

    Delete code directory

--- code/code.js
+++
@@ -1,6 +0,0 @@
-// 1x1 Map
-let img = new Image;
-img.src = 'https://github.com/carlgustavh/GoogleSnakeCustomMenuStuffImages/blob/main/Micro.png?raw=true';
-img.width = 3;
-img.height = 3;
-document.querySelector('#size').appendChild(img);
