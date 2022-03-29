42026e67f86ea917a8f8c4d37226de3b253dfc50
Booze <61169560+MBooze@users.noreply.github.com>
2022-03-28 20:55:30 -0400

    Delete code.js

--- code.js
+++
@@ -1,6 +0,0 @@
-// Micro map (test)
-let img = new Image;
-img.src = 'https://github.com/carlgustavh/GoogleSnakeCustomMenuStuffImages/blob/main/Micro.png?raw=true';
-img.width = 47;
-img.height = 47;
-document.querySelector('#size').appendChild(img);
